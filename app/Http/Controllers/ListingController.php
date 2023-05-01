449fdbf1753a2ed30b0dfb3d857c045a250cb372
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-05-01 23:35:21 +0700

    laravel crash course traversy media

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    // show all listings
+    public function index()
+    {
+        return view('listings.index', [
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->simplePaginate(4)
+        ]);
+    }
+
+    // show a single listing
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    // show create form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // store a new listing
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'description' => 'required',
+            'tags' => 'required',
+            'email' => 'required|email',
+            'website' => 'required',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Your listing has been added!');
+    }
+}
---
+++ app/Models/Listing.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'company', 'location', 'description', 'tags', 'email', 'website', 'logo'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        if ($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('company', 'like', '%' . request('search') . '%')
+                ->orWhere('location', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%');
+        }
+    }
+}
